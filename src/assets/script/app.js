23c017f7a536e29e0099fe1ea58edd3b8167239c
ia5go <benvindoiago@outlook.com>
2024-07-13 22:27:09 -0300

    parcel setup

--- src/assets/script/app.js
+++ src/assets/script/app.js
@@ -1,1 +1,8 @@
-alert("js funcionando");
+// import "../../../node_modules/@splidejs/splide/dist/css/themes/splide.min.css";
+import Splide from "@splidejs/splide";
+
+new Splide(".splide", {
+  perPage: 3,
+  pagination: false,
+  arrows: false,
+}).mount();
